480839804b552854d6cec61717ac898189892f54
Andrei Punko <andd3dfx@gmail.com>
2022-04-20 10:34:34 +0300

    Add UnsecureRestConfig to make calls without certificate check.
    Use profile 'unsecure' to start 'Facade service' with this config

--- facade-service/src/main/java/by/andd3dfx/templateapp/configs/UnsecureRestConfig.java
+++ facade-service/src/main/java/by/andd3dfx/templateapp/configs/UnsecureRestConfig.java
@@ -1,10 +1,12 @@
 package by.andd3dfx.templateapp.configs;
 
+import java.security.cert.X509Certificate;
 import javax.net.ssl.SSLContext;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,20 +15,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-@Profile("!unsecure")
+@Profile("unsecure")
 @Configuration
-public class RestConfig {
-
-    @Value("${trust.store.location}")
-    private Resource trustStore;
-
-    @Value("${trust.store.password}")
-    private String trustStorePassword;
+public class UnsecureRestConfig {
 
     @Bean
     public RestTemplate restTemplate() throws Exception {
+        TrustStrategy trustStrategy = (X509Certificate[] chain, String authType) -> true;;
         SSLContext sslContext = new SSLContextBuilder()
-            .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
+            .loadTrustMaterial(trustStrategy)
             .build();
 
         SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
