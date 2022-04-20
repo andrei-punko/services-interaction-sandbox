a2ca095cb708ca91ac82fc3608293b780e6f6d15
Andrei Punko <andd3dfx@gmail.com>
2022-04-20 11:42:42 +0300

    Adjust bat script used to run services locally with cert issued for localhost

--- articles-service/run_via_maven.bat
+++ articles-service/run_via_maven.bat
@@ -1,2 +1,2 @@
 
-mvn spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=h2
+mvn spring-boot:run -Dspring-boot.run.arguments="--spring.profiles.active=h2 --server.ssl.key-store=file:../_cert/issued-for-localhost/andd3dfx.p12"
--- facade-service/run_via_maven.bat
+++ facade-service/run_via_maven.bat
@@ -1,2 +1,2 @@
 
-mvn spring-boot:run
+mvn spring-boot:run -Dspring-boot.run.arguments="--trust.store.location=file:../_cert/issued-for-localhost/andd3dfx.p12"
